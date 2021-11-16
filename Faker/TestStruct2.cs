049f504d8efd4a6e32af7bec9a79f6bc1abd0301
Vladimir <lililolo1222@mail.ru>
2021-11-16 20:55:23 +0300

    12 commit Add TestStruct2

---
+++ Faker/TestStruct2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    class TestStruct2
+    {
+        public int a;
+        public string b;
+        public DateTime c;
+    }
+}

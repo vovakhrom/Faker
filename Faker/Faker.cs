18b1b44a1304a3af6d1019875c9fcddaf66bff6c
Vladimir <lililolo1222@mail.ru>
2021-11-16 20:47:43 +0300

    2 commit randoms

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -8,6 +8,17 @@ namespace Faker
 {
     class Faker: IFaker
     {
-        
+        private Random r = new Random();
+
+        private const int stringLength = 100;
+        private const char minChar = '0';
+        private const int maxChar = 'z';
+        private DateTime minDateTime = new DateTime(1970, 1, 1);
+        private const int maxDateSeconds = 60 * 365 * 24 * 60 * 60;
+        private const int listCount = 10;
+
+        public Faker()
+        {
+        }
     }
 }

b28ce26bd4a81c54b81f2bbe4e17cdc7ccf9821c
Vladimir <lililolo1222@mail.ru>
2021-11-16 20:54:51 +0300

    11 commit Add TestStruct

---
+++ Faker/TestStruct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    class TestStruct
+    {
+        public int a;
+        public string b;
+        public DateTime c;
+
+        public TestStruct()
+        {
+            this.b = "123";
+
+        }
+
+        public TestStruct(int a)
+        {
+            this.a = a;
+            this.b = ";;;";
+            this.c = DateTime.Now;
+        }
+    }
+}

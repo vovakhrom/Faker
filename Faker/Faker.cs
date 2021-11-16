764608ef2c066dcf09f50ea0db299173f7c620e2
Vladimir <lililolo1222@mail.ru>
2021-11-16 20:51:55 +0300

    7 commit CreateRandomObject

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -78,5 +78,62 @@ namespace Faker
                 result.Add(faker.Create(elementType));
             return result;
         }
+        
+        private object CreateRandomObject(Type type)
+        {
+            ConstructorInfo[] constructors = type.GetConstructors();
+            if (constructors.Length != 0)
+            {
+                while (true)
+                {
+                    int num = 0;
+                    for (int i = 1; i < constructors.Length; i++)
+                        if (constructors[i] != null)
+                        {
+                            if ((constructors[i] == null) || 
+                                (constructors[i].GetParameters().Length > 
+                                 constructors[num].GetParameters().Length))
+                            {
+                                num = i;
+                            }
+                        }
+
+                    if (constructors[num] == null) break;
+
+                    ParameterInfo[] paramInfo = constructors[num].GetParameters();
+                    object[] param = new object[paramInfo.Length];
+
+
+                    for (int i = 0; i < param.Length; ++i)
+                    {
+                        Type t = paramInfo[i].ParameterType;
+                        param[i] = (new Faker()).Create(t);
+                    }
+
+                    object result;
+                    try
+                    {
+                        result = Activator.CreateInstance(type, param);
+                        FillFields(result, type);
+                        FillProperties(result, type);
+                    }
+                    catch
+                    { 
+                        result = null; 
+                    }
+
+                    if (result != null)
+                        return result;
+
+                    constructors[num] = null;
+                }
+                return null;
+            }
+            else
+            {
+                return Activator.CreateInstance(type);
+            }
+        }
+
     }
 }

0d303a63e93204958b5bc2b23b4680840130992c
Vladimir <lililolo1222@mail.ru>
2021-11-16 20:55:54 +0300

    13 commit Add TestClass

---
+++ Faker/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    class TestClass
+    {
+        public int a;
+        public string b;
+        public DateTime c;
+
+        public int A { get; set; }
+
+        public TestClass (int a)
+        {
+            this.a = a;
+            this.b = "abc";
+        }
+
+        public TestClass(int a, DateTime c)
+        {
+            this.a = a;
+            this.b = "xyz";
+        }
+    }
+}
